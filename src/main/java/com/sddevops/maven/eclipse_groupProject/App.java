3fb5418e72e45541ec472ea7bfbaa7cdb2f481d8
Vishvahh <2102133B@student.tp.edu.sg>
2022-11-07 11:02:04 +0800

    initial commit

---
+++ src/main/java/com/sddevops/maven/eclipse_groupProject/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven.eclipse_groupProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
